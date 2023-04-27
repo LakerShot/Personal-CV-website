6751bdb086263de3605778b6bbd157e73a6fab0c
Egor <48025722+LakerShot@users.noreply.github.com>
2023-04-27 18:11:49 +0700

    chore: move generated code

---
+++ src/ts/main.ts
@@ -0,0 +1,13 @@
+import 'normalize.css';
+import '../styles/scss/main.scss'
+// import viteLogo from '/public/vite.svg'
+
+// document.querySelector<HTMLDivElement>('#app')
+
+const initProject = () => {
+    console.log('app is running...')
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    initProject()
+})
