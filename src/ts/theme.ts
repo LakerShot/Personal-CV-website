9f85945e5c9e607f1e7456c615ef328ac6f366d1
egor_morozov <kriskyle4130@gmail.com>
2023-05-01 02:01:47 +0700

    refactor: js logo

--- src/ts/theme.ts
+++ src/ts/theme.ts
@@ -1,20 +1,20 @@
 const themeButton = document.querySelector<HTMLButtonElement>('.portfolio__theme-btn');
 const themeIcon = document.querySelector<HTMLImageElement>('.portfolio__theme-icon');
 const html = document.querySelector<HTMLHtmlElement>('html');
+
 export const initThemeMode = () => {
-    const toggleThemeHandler = event => {
+    const toggleThemeHandler = () => {
         const theme = html.dataset.theme;
 
         if (theme) {
             html.dataset.theme = '';
             themeIcon.src = '/src/assets/svg/bulb.svg';
             localStorage.setItem('theme-mode', 'light');
-            return;
+        } else {
+            html.dataset.theme = 'light';
+            themeIcon.src = '/src/assets/svg/bulb-light.svg';
+            localStorage.setItem('theme-mode', 'dark');
         }
-
-        html.dataset.theme = 'light';
-        themeIcon.src = '/src/assets/svg/bulb-light.svg';
-        localStorage.setItem('theme-mode', 'dark');
     }
 
     themeButton.addEventListener('click', toggleThemeHandler)
