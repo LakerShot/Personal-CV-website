71e04aac6a1b975dfa9ab336612693750d05848b
egor_morozov <kriskyle4130@gmail.com>
2023-05-02 03:37:34 +0700

    refactor: improve navigation.ts

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,7 +1,20 @@
 const navigationContainer = document.querySelector<HTMLUListElement>('.navigation');
 const navigationLinks = [...document.querySelectorAll('.navigation__link')]
+const logoLink = document.querySelector<HTMLLinkElement>('.portfolio__logo');
+const aboutLink = document.querySelector<HTMLLinkElement>('.navigation__about')
 
 export const initNavigation = () => {
+
+    const resetActiveLink = () => {
+        navigationLinks.forEach(navigationLink => {
+            navigationLink.classList.remove('navigation__link--active')
+        })
+    }
+    const handleLogoLink = () => {
+        resetActiveLink();
+        aboutLink.classList.add('navigation__link--active');
+    }
+
     const navigationHandler = event => {
         const selectedLink = event.target as HTMLLinkElement;
 
@@ -9,13 +22,12 @@ export const initNavigation = () => {
             return;
         }
 
-        navigationLinks.forEach(navigationLink => {
-            navigationLink.classList.remove('navigation__link--active')
-        })
+        resetActiveLink();
 
         selectedLink.classList.add('navigation__link--active')
     }
 
-    navigationContainer.addEventListener('click', navigationHandler)
+    logoLink.addEventListener('click', handleLogoLink)
+    navigationContainer.addEventListener('click', navigationHandler);
 }
 
