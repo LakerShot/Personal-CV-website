a413b9d81714fec9a9a325245ebb5ae6f5fe9299
egor_morozov <kriskyle4130@gmail.com>
2023-05-03 00:41:06 +0700

    refactor: improve carousel

--- src/ts/education-carousel.ts
+++ src/ts/education-carousel.ts
@@ -1,8 +1,10 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
 
 export const initEducationCarousel = (): void => {
-    const educationCarousel = new Swiper<Swiper>('.education__carousel', {
+    new Swiper<Swiper>('.education__carousel', {
         direction: "vertical",
+        spaceBetween: 40,
+        slidesPerView: 1,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
