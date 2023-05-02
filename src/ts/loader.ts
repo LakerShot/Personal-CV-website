231d5cb5bc07e88d36e3ee50c733c0cac21ba20b
egor_morozov <KrisKyle4130@yandex.ru>
2023-05-02 16:48:05 +0700

    refactor: improve a bit loader

--- src/ts/loader.ts
+++ src/ts/loader.ts
@@ -1,6 +1,11 @@
 const loaderContainer = document.querySelector<HTMLDivElement>('.portfolio__loader');
-const content = document.querySelector<HTMLDivElement>('.content-container')
+const content = document.querySelector<HTMLDivElement>('.app-container');
+
 export const hideLoader = () => {
-    content.classList.remove('app-container--hidden');
-    loaderContainer.classList.add('portfolio__loader--hidden')
+    content.classList.remove('portfolio__content--hidden');
+    content.classList.add('portfolio__content');
+    content.style.display = 'block';
+
+    loaderContainer.style.display = 'none';
+    loaderContainer.classList.add('portfolio__loader--hidden');
 }
