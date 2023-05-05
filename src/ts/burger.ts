fea2ffd0667d1120f8dc8bf419d1b52d7e27a160
egor_morozov <KrisKyle4130@yandex.ru>
2023-05-05 20:00:26 +0700

    feat: implement burger-menu

--- src/ts/burger.ts
+++ src/ts/burger.ts
@@ -1,8 +1,10 @@
 const burger = document.querySelector<HTMLButtonElement>('.burger__button');
+const burgerMenu = document.querySelector<HTMLElement>('.burger-menu')
 
 export const initBurger = (): void => {
     const burgerHandler = () => {
         burger.classList.toggle('burger__button--opened');
+        burgerMenu.classList.toggle('burger-menu--opened');
     }
 
     burger.addEventListener('click', burgerHandler)
