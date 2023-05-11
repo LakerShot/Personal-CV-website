c20270c5e21d63b8ec220a7d09600f81d68b3561
egor_morozov <KrisKyle4130@yandex.ru>
2023-05-11 13:14:25 +0700

    fix: localStorage theme

--- src/ts/theme.ts
+++ src/ts/theme.ts
@@ -5,7 +5,23 @@ const html = document.querySelector<HTMLHtmlElement>('html');
 
 const themePrefix = 'theme-mode'
 
+const permittedThemes = {
+    DARK: 'dark',
+    LIGHT: 'light'
+}
+
 export const initThemeMode = () => {
+    const handleThemeOnLoad = () => {
+        const currentTheme = localStorage.getItem(themePrefix);
+        if (currentTheme === permittedThemes.LIGHT) {
+            themeIcon.classList.remove('portfolio__theme-icon--default')
+            themeIcon.classList.add('portfolio__theme-icon--light')
+            html.dataset.theme = currentTheme;
+        } else {
+            html.dataset.theme = ''
+        }
+    }
+
     const toggleThemeHandler = () => {
         const theme = html.dataset.theme;
 
@@ -13,15 +29,16 @@ export const initThemeMode = () => {
             html.dataset.theme = '';
             themeIcon.classList.remove('portfolio__theme-icon--light')
             themeIcon.classList.add('portfolio__theme-icon--default')
-            localStorage.setItem(themePrefix, 'dark');
+            localStorage.setItem(themePrefix, permittedThemes.DARK);
         } else {
             html.dataset.theme = 'light';
             themeIcon.classList.remove('portfolio__theme-icon--default')
             themeIcon.classList.add('portfolio__theme-icon--light')
-            localStorage.setItem(themePrefix, 'light');
+            localStorage.setItem(themePrefix, permittedThemes.LIGHT);
         }
     }
 
+    handleThemeOnLoad()
     headerThemeButton.addEventListener('click', toggleThemeHandler)
     asideThemeButton.addEventListener('click', toggleThemeHandler)
 }
